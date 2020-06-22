1327007b045c507b385f078b7f423f39397faac0
chechasch <sschlotfeldt@miuandes.cl>
2020-06-22 15:08:31 -0400

    name y lastname

--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -26,8 +26,9 @@ class User < ApplicationRecord
 
     # Uncomment the section below if you want users to be created if they don't exist
      unless user
-         user = User.create(name: data['name'].map[0],
-                            lastName: data['name'].map[1],
+         user = User.create(name: data['name'].split(" ")[0].capitalize,
+                            lastName: data['name'].split(" ")[1].capitalize,
+                            userName: data['email'].split("@")[0].capitalize,
                             email: data['email'],
                             user_pic: data['picture'] ,
                             password: Devise.friendly_token[0,20]
