2b874548572f0c04da9ff09b6f3fc2ca7bb3b542
Web Tech <fjbarros1@miuandes.cl>
2020-06-25 03:41:29 -0400

    minor bugs fixed Comments! OK

--- Project/app/controllers/comments_controller.rb
+++ Project/app/controllers/comments_controller.rb
@@ -44,7 +44,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.'}
+        format.html { redirect_to comments_path, notice: 'Comment was successfully created.'}
         format.json { render :show, status: :created, location: @comment }
       else
         format.html { render :new }
@@ -85,7 +85,10 @@ class CommentsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+      begin
+        @comment = Comment.find(params[:id])
+      rescue
+      end
     end
 
     # Only allow a list of trusted parameters through.
