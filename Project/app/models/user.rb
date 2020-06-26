c6ed386a1941200b620d35fb9e2778fbf318d886
chechasch <sschlotfeldt@miuandes.cl>
2020-06-26 03:24:45 -0400

    styled

--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable
 
   has_many :events
-  has_many :organizations
+  has_one :organization
   has_one :inbox
   has_many :messages
   has_many :organizationMs, dependent: :destroy
